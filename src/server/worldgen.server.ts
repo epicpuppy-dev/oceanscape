c627a34440b64ad3897ddfc88315f9a04767a337
EpicPuppy613 <epicpuppy@epicpuppy.dev>
2024-06-18 16:04:25 -0700

    Implement disc generation algorithm

--- src/server/worldgen.server.ts
+++ src/server/worldgen.server.ts
@@ -1,9 +1,9 @@
 import { InsertService, Workspace } from "@rbxts/services";
-import { haltonSequence } from "shared/gen";
+import { discAlgorithm, haltonSequence } from "shared/gen";
 
-function generateIsland(x: number, y: number) {
+function generateIsland(parent: Instance, x: number, y: number) {
     const island = InsertService.LoadAsset(17900649471).GetChildren()[0] as Model;
-    island.Parent = Workspace;
+    island.Parent = parent;
     //Move island randomly
     island.MoveTo(new Vector3(x, -3, y));
     //Set random rotation
@@ -14,16 +14,32 @@ function generateIsland(x: number, y: number) {
     return island;
 }
 
-function generateWorld(seed: number, size: number, bases: number) {
+function generateWorld(bases: [number, number][]) {
     const islandList = [];
-    const lowerBound = math.floor(size / 2);
+    const folder = new Instance("Folder");
+    folder.Name = "Islands";
+    folder.Parent = Workspace;
     // Always ensure that an island is generated nearby the origin
-    islandList.push(generateIsland(math.floor(math.random() * 600) - 300, math.floor(math.random() * 600) - 300));
-    for (let i = 0; i < bases - 1; i++) {
-        islandList.push(
-            generateIsland(haltonSequence(i, 2) * size - lowerBound, haltonSequence(i, 3) * size - lowerBound),
-        );
+    for (let i = 0; i < bases.size(); i++) {
+        islandList.push(generateIsland(folder, bases[i][0], bases[i][1]));
     }
 }
 
-generateWorld(0, 4000, 20);
+function generateDiscWorld(
+    seed: number,
+    size: number,
+    discs: number,
+    radii: number[],
+    separation: number[],
+    points: number[],
+    variance: number[],
+) {
+    const bases = discAlgorithm(seed, size, discs, radii, separation, points, variance);
+    bases.map((base) => {
+        base[0] = base[0] - size / 2;
+        base[1] = base[1] - size / 2;
+    });
+    generateWorld(bases);
+}
+
+generateDiscWorld(0, 6000, 3, [100, 2000, 4000], [0, 75, 40], [1, 3, 5], [100, 1000, 2000]);
--- src/shared/gen.ts
+++ src/shared/gen.ts
@@ -8,3 +8,46 @@ export function haltonSequence(index: number, base: number) {
     }
     return r;
 }
+
+export function discAlgorithm(
+    seed: number,
+    size: number,
+    discs: number,
+    radii: number[],
+    separation: number[],
+    points: number[],
+    variance: number[],
+) {
+    const output: [number, number][] = [];
+    math.randomseed(seed);
+    // for each disc
+    for (let i = 0; i < discs; i++) {
+        // generate an array of degrees with the minimum separation
+        const degrees: number[] = [];
+        for (let j = 0; j < points[i]; j++) {
+            let point = math.random() * 360;
+            while (
+                degrees.some((degree) => {
+                    const distance = math.min(
+                        math.abs(degree - point),
+                        math.abs(degree - point + 360),
+                        math.abs(degree - point - 360),
+                    );
+                    return distance < separation[i];
+                })
+            ) {
+                point = math.random() * 360;
+            }
+            degrees.push(point);
+        }
+
+        // draw each of the points, including the variance
+        for (let j = 0; j < points[i]; j++) {
+            const x = size / 2 + math.cos(math.rad(degrees[j])) * radii[i] + (math.random() - 0.5) * variance[i];
+            const y = size / 2 + math.sin(math.rad(degrees[j])) * radii[i] + (math.random() - 0.5) * variance[i];
+            output.push([x, y]);
+        }
+    }
+
+    return output;
+}
